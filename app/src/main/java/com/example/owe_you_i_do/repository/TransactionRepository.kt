3f85c72af9b34bd78e74990de952cc3a2e9c9995
dibin <dibin.nadar@jiva.ag>
2022-08-07 17:43:45 +0530

    Added repository for Transactions

---
+++ app/src/main/java/com/example/owe_you_i_do/repository/TransactionRepository.kt
@@ -0,0 +1,22 @@
+package com.example.owe_you_i_do.repository
+
+import com.example.owe_you_i_do.db.Transaction
+import com.example.owe_you_i_do.db.TransactionDAO
+
+class TransactionRepository(private val transactionDAO: TransactionDAO) {
+
+    suspend fun insertTransaction(transaction: Transaction) =
+        transactionDAO.insertTransaction(transaction)
+
+    suspend fun updateTransaction(transaction: Transaction) =
+        transactionDAO.updateTransaction(transaction)
+
+    suspend fun deleteTransaction(transaction: Transaction) =
+        transactionDAO.deleteTransaction(transaction)
+
+    suspend fun deleteAllTransactions() =
+        transactionDAO.deleteAllTransactions()
+
+    fun getAllTransaction() =
+        transactionDAO.getAllTransaction()
+}
